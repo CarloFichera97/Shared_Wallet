aade341bb3fc0ec9475b9edc0bec50e3a8524470
Carlo Fichera <Carlo.fichera97@gmail.com>
2021-05-16 19:17:37 +0100

    General Update
    Added various function, modified and added functionality to withdrawPartialMoney function

--- Main.sol
+++ Main.sol
@@ -5,15 +5,15 @@ pragma solidity >0.5.13;
 
 contract Shared_Wallet {
     
-    address payable owner;
-    int allowance;
+    address owner;
+    int allowance = 100;
+    mapping(address => uint) public balanceReceived;
+    mapping(address => uint) TimeCount;
+    mapping(address => uint) countAmountWithdrawn;
     
-    constructor () public {
+    constructor () {
         owner = msg.sender;
     }
-    
-    mapping(address => uint) public balanceReceived;
-    
 
     function getOwner() public view returns(address) {
         return owner;
@@ -28,10 +28,24 @@ contract Shared_Wallet {
      function getAllowance() public view returns(uint) {
          return uint(allowance);
      }
+
+    function getAmountWithdrawn() public view returns(uint) {
+         return uint(countAmountWithdrawn[msg.sender]);
+     }
+
+     function TestTime() public view returns(uint) {
+         uint Time = TimeCount[msg.sender] - block.timestamp;
+         if (Time >180) {
+             return(0);
+         }
+         else {
+            return uint(Time);
+         }
+    }
      
      function setAllowance(int _allowance) public payable {
          require(msg.sender == owner, "You are not the owner and cannot change the allowance value");
-         require(_allowance > 0, "The allowance set must be positive");
+         require(_allowance >= 0, "The allowance set must be positive");
          allowance = _allowance;
      }
      
@@ -45,18 +59,29 @@ contract Shared_Wallet {
          _to.transfer(getBalance());
      }
      
-     //Require further implementation for withdrawing a desired amount of money smaller than the allowance
-     //Note. It is necessary that the user is not allowed to withdraw an amount lower than the allowance multiple times and it is necessary
-     //to define the function such that the sum over the total withdrawals is <= Allowance function (address payable _to, uint _amount, uint _allowance)public
-     function withdrawPartialMoney(address payable _to) public {
-         _to.transfer(getAllowance());
-         
+     //This function allow the user to withdraw a certain amount of money during a certain amount of Time
+     //The maximum amount of money withdrawable depends on the allowance which is set by the contract owner
+     //The user can withdraw more than one time during the set period as long as the sum of al the withdrawals are <= allowance 
+     function withdrawPartialMoney(address payable _to, uint _amount) public payable {
+         require(_amount <= getAllowance(), "You cannot withdraw more than your allowance");
+         require(_amount <= getBalance(), "There are not enough funds Locked in the Smart Contract");
+         if (msg.sender == owner){
+            _to.transfer(_amount);
+         } else {
+             if (TimeCount[msg.sender] < block.timestamp) {
+                 countAmountWithdrawn[msg.sender] = 0;
+                 TimeCount[msg.sender] = 0;
+                 }
+             if (countAmountWithdrawn[msg.sender] == 0) {
+                 TimeCount[msg.sender] = block.timestamp + 180;
+                 }
+             countAmountWithdrawn[msg.sender] = countAmountWithdrawn[msg.sender] + _amount;
+             require(countAmountWithdrawn[msg.sender] <= getAllowance(), "You are not allowed to withdraw more than your allowance");
+             _to.transfer(_amount);
+             }
      }
-     
     function destroySmartContract(address payable _to) public {
-        
         require(msg.sender == owner, "You are not the owner");
         selfdestruct(_to);
-    }
-     
+    } 
 }
